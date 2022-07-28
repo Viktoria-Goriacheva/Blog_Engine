d82156b4ff2073cd5e36688e7c1a54bf76bf01cb
Viktoria-Goriacheva <72920169+Viktoria-Goriacheva@users.noreply.github.com>
2022-07-28 13:32:24 +0300

    Delete main/repository/GlobalSettingsRepository.java

--- main/repository/GlobalSettingsRepository.java
+++
@@ -1,8 +0,0 @@
-package main.repository;
-
-import main.model.GlobalSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GlobalSettingsRepository extends JpaRepository<GlobalSettings, Integer> {
-
-}
