7b743db94908ff3b430d35eb8b5c330b18fff0a9
Viktoria-Goriacheva <72920169+Viktoria-Goriacheva@users.noreply.github.com>
2022-07-28 13:32:34 +0300

    Delete main/repository/PostVoteRepository.java

--- main/repository/PostVoteRepository.java
+++
@@ -1,8 +0,0 @@
-package main.repository;
-
-import main.model.PostVote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostVoteRepository extends JpaRepository<PostVote, Integer> {
-
-}
