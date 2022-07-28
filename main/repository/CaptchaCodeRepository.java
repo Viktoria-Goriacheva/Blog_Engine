05798ecabee3b3b030751d9a210de1452d79ce0a
Viktoria-Goriacheva <72920169+Viktoria-Goriacheva@users.noreply.github.com>
2022-07-28 13:32:19 +0300

    Delete main/repository/CaptchaCodeRepository.java

--- main/repository/CaptchaCodeRepository.java
+++
@@ -1,8 +0,0 @@
-package main.repository;
-
-import main.model.CaptchaCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CaptchaCodeRepository extends JpaRepository<CaptchaCode, Integer> {
-
-}
